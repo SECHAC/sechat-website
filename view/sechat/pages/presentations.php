757ea82cbfc7dea4170a4886053984a63e2cb5cd
SpaceLenore <libreterra@protonmail.com>
2019-02-04 17:43:08 +0100

    completed presentation page

--- view/sechat/pages/presentations.php
+++ view/sechat/pages/presentations.php
@@ -9,13 +9,13 @@
     </div>
     <div class="combine-box">
     <!-- <div class="halfscreen"> -->
-        <div class="center-content regular-panel">
+        <div class="center-content regular-panel txt-center">
             <h2 class="">Call for presentations</h2>
             <p>Vill DU vara med och föreläsa på SECHAT? Skicka in ett melj till oss!</p>
-            <a href="mailto:sechat.papers@dbwebb.se" class="very-fancy-button small-bt">Mejla oss</a>
+            <a href="mailto:sechat.papers@dbwebb.se" class="very-fancy-button small-bt center-bt">Mejla oss</a>
         </div>
         <div class="center-content regular-panel">
-            <h2 class="txt-center">Highlight Speakers</h2>
+            <h2 class="txt-center">Highlighted Speakers</h2>
             <div class="speakers">
                 <div class="speaker">
                     <img src="//placehold.it/100x100" alt="Profil">
@@ -43,6 +43,55 @@
                     <span><i>Talets titel här</i> </span>
                 </div>
             </div>
+            <h2 class="txt-center">Fullständigt schema</h2>
+            <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                <thead>
+                    <tr>
+                        <th>Dag</th>
+                        <th>Tid</th>
+                        <th>Talare</th>
+                        <th>Presentation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Fredag</td>
+                        <td>09:00-19:30</td>
+                        <td>Mikael Roos</td>
+                        <td>Öppnignstal SECHAT19</td>
+                    </tr>
+                    <tr>
+                        <td>Fredag</td>
+                        <td>09:30-10:00</td>
+                        <td>Talare Talarsson</td>
+                        <td>Komplett devops med GitLab</td>
+                    </tr>
+                    <tr>
+                        <td>Fredag</td>
+                        <td>10:15-10:30</td>
+                        <td>daBack</td>
+                        <td>CSS-Grid</td>
+                    </tr>
+                    <tr>
+                        <td>Fredag</td>
+                        <td>10:30-10:45</td>
+                        <td>Emil Folino</td>
+                        <td>När man ska (och inte ska) använda ramverk</td>
+                    </tr>
+                    <tr>
+                        <td>Fredag</td>
+                        <td>11-13</td>
+                        <td></td>
+                        <td>Lunchpaus</td>
+                    </tr>
+                    <tr>
+                        <td>Fredag</td>
+                        <td>13-14</td>
+                        <td>wip17</td>
+                        <td>Gemensam föreläsning från wip17</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
