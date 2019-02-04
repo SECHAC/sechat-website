ee0bc803db7a272ce181c56c28120797f5abe9f6
SpaceLenore <libreterra@protonmail.com>
2019-02-04 17:10:57 +0100

    added footer

--- view/sechat/components/footer/material.php
+++ view/sechat/components/footer/material.php
@@ -1,4 +1,48 @@
-
 </div>
 </main>
 </div>
+<footer class="mdl-mega-footer">
+  <div class="mdl-mega-footer__middle-section">
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading">SECHAT 2019</h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li>
+            GrillCon är ett event som sker i slutet av maj och tre veckor in i september.
+            Det är en grillfest för webbprogrammerare, programvarutekniker och likasinnade.
+        </li>
+      </ul>
+    </div>
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading">Sociala Media</h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="https://www.facebook.com/GrillCon">Facebook</a></li>
+        <li><a href="https://www.twitter.com/GrillCon">Twitter</a></li>
+        <li><a href="https://plus.google.com/u/0/b/106688549010851146409/106688549010851146409">Google+</a></li>
+        <li><a href="https://www.linkedin.com/groups/8518022">LinkedIn</a></li>
+        <li><a href="https://github.com/dbwebb-se/grillcon">GitHub</a></li>
+      </ul>
+    </div>
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading">Join Us</h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="irc.bsnet.se://#db-o-webb">irc.bsnet.se://#db-o-webb</a></li>
+        <li><a href="https://github.com/dbwebb-se/grillcon/issues">GitHub Issues</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="mdl-mega-footer__bottom-section">
+    <div class="mdl-logo">SECHAT 2019</div>
+    <ul class="mdl-mega-footer__link-list">
+      <li>Copyright &copy; 2019</li>
+      <li><a href="#">Källkod</a></li>
+    </ul>
+  </div>
+
+</footer>
